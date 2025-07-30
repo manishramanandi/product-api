1ec669d68ee9e0182b2da82fcab25d0b520b2120
manishramanandi <bairagimanish222@gmail.com>
2025-07-30 17:30:16 +0530

    feat: Initial Commit

---
+++ src/router.ts
@@ -0,0 +1,58 @@
+import { Router } from "express";
+import { body, oneOf, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
+import { createProduct, deleteProduct, getOneProduct, getProducts } from "./handlers/products";
+
+const router = Router();
+
+/*
+ * Product
+ */
+
+router.get("/product", getProducts)
+router.get("/product/:id", getOneProduct)
+router.post("/product",body("name").isString(),handleInputErrors, createProduct)
+router.put("/product/:id", body("name").isString(),handleInputErrors, (req, res) => {
+});
+router.delete("/product/:id",deleteProduct);
+
+/*
+ * Update
+ */
+
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.post("/update",
+    body('title').exists().isString(),
+    body('body').exists().isString(),
+    body('productId').exists().isString(),
+    () => {}
+);
+router.put("/update/:id",
+    body('title').optional(),
+    body('body').optional(),
+    body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
+    body('version').optional(),
+    () => {}
+);
+router.delete("/update/:id", () => {});
+
+/*
+ * Update Point
+ */
+
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.post("/updatepoint",
+    body('name').exists().isString(), 
+    body('Description').exists().isString(),
+    body('updateId').exists().isString(),
+    () => {}
+);
+router.put("/updatepoint/:id", 
+    body('name').optional().isString(), 
+    body('Description').optional().isString(),
+    () => {});
+router.delete("/updatepoint/:id", () => {});
+
+export default router;
